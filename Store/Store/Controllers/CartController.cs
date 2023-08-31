510a481ad314745da8cd474fad4ead6b575b2fe2
TheGhost025 <mazenkaluoby111@gmail.com>
2023-08-31 15:40:17 +0300

    Coferm Cart

--- Store/Store/Controllers/CartController.cs
+++ Store/Store/Controllers/CartController.cs
@@ -68,5 +68,55 @@ namespace Store.Controllers
             ViewData["Products"] = context.Product.ToList();
             return View("CartView",cart1);
         }
+
+        public IActionResult Delete(int id) 
+        {
+            Cart cart = context.Cart.FirstOrDefault(x => x.Id==id);
+            context.Cart.Remove(cart);
+            context.SaveChanges();
+            int? id1 = HttpContext.Session.GetInt32("ID");
+            List<Cart> cart1 = context.Cart.Where(x => x.cust_id == id1).ToList();
+            ViewData["Products"] = context.Product.ToList();
+            return View("CartView", cart1);
+        }
+
+        public IActionResult ConfermCart(int id)
+        {
+            List<Cart> cart = context.Cart.Where(x => x.cust_id == id).ToList();
+            List<ConfermCart> confermCarts = cart.Select(c => new ConfermCart
+            {
+                cust_id = c.cust_id,
+                prod_id = c.prod_id,
+                Quantity = c.Quantity,
+                TotalPrice = CalculateTotalPrice(c.prod_id, c.Quantity),
+                PurchaseDate = DateTime.Now
+            }).ToList();
+
+            context.ConfermCart.AddRange(confermCarts);
+            context.Cart.RemoveRange(cart);
+            context.SaveChanges();
+
+            int? id1 = HttpContext.Session.GetInt32("ID");
+            List<Cart> cart1 = context.Cart.Where(x => x.cust_id == id1).ToList();
+            ViewData["Products"] = context.Product.ToList();
+            return View("CartView",cart1);
+        }
+
+        private double CalculateTotalPrice(int productId, int quantity)
+        {
+            double productPrice = GetProductPrice(productId,quantity);
+            double totalPrice = productPrice * quantity;
+            return totalPrice;
+        }
+
+        private double GetProductPrice(int productId,int quantity)
+        {
+            Product product = context.Product.FirstOrDefault(x => x.Id == productId);
+            product.Quantity -= quantity;
+            context.Product.Update(product);
+
+            double productPrice = context.Product.FirstOrDefault(x => x.Id == productId).Price;
+            return productPrice;
+        }
     }
 }
